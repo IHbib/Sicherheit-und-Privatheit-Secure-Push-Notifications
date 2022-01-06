4a92609f87745f77be25cab69875e9c268c21760
Issam Hbib <37027794+IHbib@users.noreply.github.com>
2022-01-06 02:47:34 +0100

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,35 @@
 from gpapi.googleplay import GooglePlayAPI
-import csv, os, subprocess, logging, sys, getopt
+from getpass import getpass
+import csv, os, subprocess, logging, sys, getopt, tempfile, shutil
 
 argumentList = sys.argv[1:]
-options = "f:l:"
-long_options = ["filename", "logname"]
+options = "f:l:L:m:T:t:g:h"
+long_options = ["filename", "logname", "Locale", "mail", "token", "help", "device", "Timezone", "gsfID"]
 logname = ""
 path = ""
+locale = "en_US"
+mail = ""
+passwd = ""
+token = ""
+timezone="UTC"
+device= "hero2lte"
+gsfID = 0
+
+def printHelp():
+    print("""-h, --help: Print current help
+            -f, --filename (REQUIRED): file path to app ID list
+            -l, --logname (REQUIRED): logname for logfile
+            -L, --Locale (optional): set locale when initializing, standard is "en_US"
+            -T, --Timezone (optional): timezone to use, standard is "UTC"
+            -d, --device (optional): device codename to use, standard: "hero2lte
+
+            Either (token AND gsfId) or mail are REQUIRED:
+            -t, --token: set token to use instead of login
+            -g, --gsfID
+            
+            -u, --mail: mail for login
+    """)
+
 try:
     # Parsing argument
     arguments, values = getopt.getopt(argumentList, options, long_options)
@@ -13,23 +37,31 @@ try:
     # checking each argument
     for currentArgument, currentValue in arguments:
  
-        if currentArgument in ("-f", "--filename"):
+        if currentArgument in ("-h", "--help"):
+            printHelp()
+            exit()
+        elif currentArgument in ("-f", "--filename"):
             path = currentValue
-             
         elif currentArgument in ("-l", "--logname"):
             logname = currentValue
+        elif currentArgument in ("-L", "--Locale"):
+            locale = currentValue
+        elif currentArgument in ("-m", "--mail"):
+            mail = currentValue
+            print("Please enter your Google Account password: ")
+            passwd = getpass()
+        elif currentArgument in ("-t", "--token"):
+            token = currentValue
+        elif currentArgument in ("-T", "--Timezone"):
+            timezone = currentValue
+        elif currentArgument in ("-d", "--device"):
+            device = currentValue
+        elif currentArgument in ("-g", "--gsfID"):
+            gsfID = currentValue
+
 except getopt.error as err:
     # output error, and return with an error code
     print (str(err))
-
-#mail = "hbrsprojektinf@gmail.com"
-#mail = "hbibissam91@gmail.com"
-#passwd = "Hbrs1234%"
-#passwd = "zislspevpxgaopwj"
-api = GooglePlayAPI(locale="en_US", timezone="UTC", device_codename="hero2lte")
-#requests 2.20.0 needed
-#api.login(email=mail, password=passwd)
-api.login(authSubToken='FAgO4ipcn9zfZkr1_-oUwSHy6G_97Zvi0jmbjHacZOSFkyT_sIb6m3JcY562sNNkoquOEQ.', gsfId=4340149443977775908)
 
 ###Logger
 logger = logging.getLogger()
@@ -47,6 +79,24 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
 
+api = GooglePlayAPI(locale=locale, timezone=timezone, device_codename=device)
+
+if mail:
+    api.login(email=mail, password=passwd)
+    if hasattr(api,"authSubToken"):
+        logger.info("Login successfully, you may want to use the following token and gsfID in the future to prevent initializing a new device with each login: ")
+        logger.info("-t "+api.authSubToken+" -m "+str(api.gsfId))
+    else:
+        logger.error("Login failed!")
+        exit()
+elif token and gsfID:
+    api.login(authSubToken=token, gsfId=gsfID)
+else:
+    logger.error("You need to enter either a token (-t, --token) AND gsfId (-g, --gsfId) or ONLY a mail (-m, --mail)!")
+    printHelp()
+    exit()
+
+
 downloadedApps = 0
 appsWithPush = 0
 withCapillary = 0
@@ -58,30 +108,30 @@ iteration = 0
 with open(path, "r") as file:
     reader = csv.reader(file)
     for idx,line in enumerate(reader):
+        tempdir = tempfile.mkdtemp()
         iteration+=1
         print("Iteration: "+str(iteration))
         docid = line[0]
-        os.makedirs(os.getcwd()+"/tmp", exist_ok=True)
         try:
             download = api.download(docid, expansion_files=True)
-            with open('tmp/'+download['docId'] + '.apk', 'wb') as first:
+            with open(tempdir+"/"+download['docId'] + '.apk', 'wb') as first:
                 for chunk in download.get('file').get('data'):
                     first.write(chunk)
         except:
             logger.error("Failed to download "+docid)
             continue
 
-        if(download['docId'] + '.apk' in os.listdir('tmp')):
+        if(download['docId'] + '.apk' in os.listdir(tempdir)):
             downloadedApps+=1
-            process = subprocess.Popen("unzip -o " +os.getcwd()+"/tmp/"+download['docId'] + '.apk'+" -d "+os.getcwd()+"/tmp/"+download['docId'], shell=True)
+            process = subprocess.Popen("unzip -o " +tempdir+"/"+download['docId'] + '.apk'+" -d "+tempdir+"/"+download['docId'], shell=True)
             out, err = process.communicate()
             process.wait()
-            if(process.returncode == -1 or not download['docId'] in os.listdir('tmp/')):
+            if(process.returncode == -1 or not download['docId'] in os.listdir(tempdir)):
                 logger.error("Failed to unzip: "+download['docId']+'.apk')
                 continue
             else:
-                pathToUse = "'{path}/tmp/{docid}'".format(path = os.getcwd(), docid=download['docId'])
-                process = subprocess.Popen("grep -rnw "+pathToUse+" -e 'NotificationCompat'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                pathToUse = "'{path}/{docid}'".format(path = tempdir, docid=download['docId'])
+                process = subprocess.Popen("grep -rnw "+pathToUse+" -e 'NotificationCompat' -e'onMessageReceived'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 process.wait()
                 out, err = process.communicate()
                 out+=err
@@ -90,10 +140,11 @@ with open(path, "r") as file:
                 except OSError:
                     # can't kill a dead proc
                     pass
-                #Wenn NotificationCompat vorhanden ist, kann die App Push Notifications erstellen und zählt somit zu der gesamten Anzahl
-                if b'NotificationCompat' in out or b'matches' in out:
+                #Wenn NotificationCompat vorhanden ist, kann die App Push Notifications empfangen und zählt somit zu der gesamten Anzahl
+                if b'onMessageReceived' in out or b'matches' in out or b'NotificationCompat' in out:
                     appsWithPush+=1
                     process = subprocess.Popen("grep -rnw "+pathToUse+" -e 'Capillary' -e 'capillary'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    #Es wird der Buffer vor dem wait geleert, da der Prozess sonst hängen bleibt, weil er darauf wartet dass der Buffer geleert wird.
                     out, err = process.communicate()
                     out+=err
                     process.wait()
@@ -110,7 +161,7 @@ with open(path, "r") as file:
                         withCapillary+=1
                         logger.info(download['docId']+" has capillary")
 
-                ######
+                    ######
                     process = subprocess.Popen("grep -rnw "+pathToUse+" -e 'xmpp' -e 'XMPP'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     out, err = process.communicate()
                     out+=err
@@ -127,18 +178,8 @@ with open(path, "r") as file:
                         withXmpp+=1
                         logger.info(download['docId']+" has xmpp")
 
-                #####
-                #Dieser Command ist gefährlich
-                process = subprocess.Popen("rm -rf "+pathToUse+"*", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                process.wait()
-                try:
-                    process.kill()
-                except OSError:
-                    # can't kill a dead proc
-                    pass
+                shutil.rmtree(tempdir)
                 logger.info("Iteration: "+str(iteration)+" downloadedApps: "+str(downloadedApps)+" appsWithPush: "+str(appsWithPush)+" withCapillary: "+str(withCapillary)+  " withXmpp: "+ str(withXmpp))
         else:
             logger.error("Download failed for: "+download['docId'] + '.apk')
-        
-
 
